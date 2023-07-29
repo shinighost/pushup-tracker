a8e6dce1f53ed2d4324bc9d5c9cdb7c90752fa8a
nitro <nitroghost@gmail.com>
2023-07-29 12:40:50 +0100

    first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [pushups, setPushups] = useState(0);
+  const [dailyPushups, setDailyPushups] = useState(0);
+
+  const handlePushupChange = (e) => {
+    setDailyPushups(Number(e.target.value));
+  };
+
+  const handlePushupSubmit = () => {
+    setPushups((prevPushups) => prevPushups + dailyPushups);
+    setDailyPushups(0);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pushup Tracker</h1>
+        <p>Total Pushups: {pushups}</p>
+        <div className="pushup-form">
+          <input
+            type="number"
+            value={dailyPushups}
+            onChange={handlePushupChange}
+            min="0"
+            placeholder="Enter your daily pushup count"
+          />
+          <button onClick={handlePushupSubmit}>Log Pushups Nitro</button>
+          
+          <br />
+          <input
+            type="number"
+            value={dailyPushups}
+            onChange={handlePushupChange}
+            min="0"
+            placeholder="Enter your daily pushup count"
+          />
+          <button onClick={handlePushupSubmit}>Log Pushups Neca</button>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
